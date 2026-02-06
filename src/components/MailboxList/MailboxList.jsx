00026bd627d4ded28f8229be32e18b06e9a69365
Jayden Davis <jaydendavis746@gmail.com>
2026-02-06 18:28:30 +0000

    I ahve added the conditional rendering

--- src/components/MailboxList/MailboxList.jsx
+++ src/components/MailboxList/MailboxList.jsx
@@ -8,10 +8,18 @@ console.log('This is the prop', props)
         <>
         <h1>MailboxList</h1>
         <div>
-            {props.mailboxes.map((currentMailbox)=>(
-               <Link to={`/mailboxes/${currentMailbox._id}`}  className="mail-box" > <strong  key={currentMailbox._id} > Mailbox {currentMailbox._id}</strong></Link>
-                
-            ))}
+            {props.mailboxes.length === 0 ? (
+                <h2>No mailboxes found</h2>
+            ) : (
+                props.mailboxes.map((currentMailbox)=>(
+               <Link to={`/mailboxes/${currentMailbox._id}`}  className="mail-box link "   > <strong  key={currentMailbox._id} > Mailbox {currentMailbox._id}</strong></Link>
+                ))
+            ) }
+
+
+
+
+            
         </div>
         </>
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,13 @@
-import { Link} from "react-router";
-
+import { Link } from "react-router";
 
 const NavBar = ()=>{
 
 
     return(
     <nav>
-    <p><Link to='/' >Home</Link></p>
-    <p><Link to='/mailboxes' >Mailboxes</Link></p>
-    <p><Link to='/new-mailbox' >Add Mailbox</Link></p>
+    <p className="link" ><Link to='/' >Home</Link></p>
+    <p className="link" ><Link to='/mailboxes' >Mailboxes</Link></p>
+    <p className="link" ><Link to='/new-mailbox' >Add Mailbox</Link></p>
     </nav>
     )
 }
